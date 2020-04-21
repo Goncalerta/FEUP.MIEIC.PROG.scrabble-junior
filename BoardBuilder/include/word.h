43ff4b1845f162d8fda979b35e1d9ac21d8e79ca
PedroGonçaloCorreia <goncalerta@gmail.com>
2020-04-21 18:41:29 +0100

    Sketch boardbuilder implementation

--- BoardBuilder/include/word.h
+++ BoardBuilder/include/word.h
@@ -1,21 +1,29 @@
 #ifndef WORD_H
 #define WORD_H
 
-using namespace std;
-
-enum Orientation {
-    Vertical,
-    Horizontal,
-}
+#include <iostream>
+#include <string>
+#include "orientation.h"
+#include "position.h"
+#include "cursor.h"
+#include "command.h"
 
 class Word {
-    int x;
-    int y;
+    static const char* DICTIONARY;
+
+    Position start;
     Orientation orientation;
-    string word;
+    std::string word;
 
     public:
-    
-}
+    Word(Position start, Orientation orientation, std::string word);
+    Word(Command cmd);
+
+    void printToStream(std::ostream &out) const;
+    std::string getWord() const;
+    Cursor getCursorAtStart() const;
+    int size() const; // unused right now
+    bool inDict() const;
+};
 
 #endif
